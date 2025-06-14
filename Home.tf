9e44bcde683b9892fe69c5f63897fd0ea8da92e3
Sangala Sreekanth <sangala2003@gmail.com>
2025-06-14 12:43:55 +0530

    Added Home file for file structer

---
+++ Home.tf
@@ -0,0 +1,16 @@
+# Example folder structure
+project/
+├── envs/
+│   ├── dev/
+│   │   ├── main.tf
+│   │   ├── variables.tf
+│   │   └── outputs.tf
+│   └── prod/
+│       ├── main.tf
+│       ├── variables.tf
+│       └── outputs.tf
+└── modules/
+    └── compute/
+        ├── main.tf
+        ├── variables.tf
+        └── outputs.tf
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 this repo for learning purpose
 <br/>
 Author Name : Sreekanth.S
+i am learning git and devops toolsstatus
+
